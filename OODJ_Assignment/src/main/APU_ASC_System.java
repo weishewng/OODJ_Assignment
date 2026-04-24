29c7416d274cab7f9292a9c6a9e1a17b67aeb4ba
weishewng <wilsonweisheng01@gmail.com>
2026-04-24 18:51:16 +0800

    Initial project structure and text file

---
+++ OODJ_Assignment/src/main/APU_ASC_System.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author USER
+ */
+public class APU_ASC_System {
+    
+}
